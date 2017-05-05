92a6faa88cb3b927249841512cf33388627420c8
grzspiteri <grzspiteri@live.com>
2017-05-05 20:58:25 +0200

    -Added an extra camera

---
+++ Assets/Scripts/ButtonLink.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonLink : MonoBehaviour
+{
+
+	public void loadLevel(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FirstPersonController : MonoBehaviour {
+
+	//movements of the player/camera
+	public float movementSpeed = 4.0f; //arrow speed
+	public float mouseSensitivity = 5.0f; //mouse speed
+
+	public int timeLeft = 240;
+	public Text countdownText;
+
+	// Use this for initialization
+	void Start()
+	{
+		StartCoroutine("LoseTime");
+	}
+	// Update is called once per frame
+	void Update (){
+		//Rotation
+		float rotLeftRight = Input.GetAxis("Mouse X") * mouseSensitivity;
+		transform.Rotate (0, rotLeftRight, 0);
+
+		//Movement
+		float forwardSpeed = Input.GetAxis ("Vertical") * movementSpeed;
+		float sideSpeed = Input.GetAxis ("Horizontal") * movementSpeed;
+		Vector3 speed = new Vector3 (sideSpeed, 0, forwardSpeed);
+
+		speed = transform.rotation * speed;
+
+		CharacterController cc = GetComponent<CharacterController> ();
+		cc.SimpleMove( speed );
+
+		//CountDown
+		countdownText.text = ("Time Left: " + timeLeft);
+
+		if (timeLeft <= 0)
+		{
+			StopCoroutine("LoseTime");
+			countdownText.text = "Times Up!";
+			SceneManager.LoadScene("GameOver");
+
+		}
+        
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "finish") {
+			SceneManager.LoadScene("YouWon");
+		}
+	}
+
+	IEnumerator LoseTime()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(1);
+			timeLeft--;
+		}
+	}
+}
